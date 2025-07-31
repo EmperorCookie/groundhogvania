9c2e66f580999216246311f38c583b86e84ae5da
DanTheDM <64277250+DanTheDM@users.noreply.github.com>
2025-07-30 17:50:03 -0700

    Add main menu script and connect button signals
    Created main_menu.gd to handle start and quit button actions. Connected button signals in main_menu.tscn and attached the script to the MainMenu node.

---
+++ menu/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_start_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://test_level/test_level.tscn")# Replace with function body.
+
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit() # Replace with function body.
